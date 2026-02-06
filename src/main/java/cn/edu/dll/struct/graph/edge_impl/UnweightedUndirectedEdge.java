699f2341bd1c8103a478add6ec851d1b4afe4380
dulei715 <dulei715@126.com>
2026-02-06 19:32:28 +0800

    Saved by Leilei through a script

--- src/main/java/cn/edu/dll/struct/graph/edge_impl/UnweightedUndirectedEdge.java
+++ src/main/java/cn/edu/dll/struct/graph/edge_impl/UnweightedUndirectedEdge.java
@@ -2,12 +2,12 @@ package cn.edu.dll.struct.graph.edge_impl;
 
 import cn.edu.dll.struct.graph.Node;
 
-public class UnweightedUndirectedEdge extends UndirectedEdge{
+public class UnweightedUndirectedEdge extends UndirectedEdge<Integer>{
     public UnweightedUndirectedEdge() {
-        super(1D);
+        super(1);
     }
 
     public UnweightedUndirectedEdge(Node nodeA, Node nodeB) {
-        super(1D, nodeA, nodeB);
+        super(1, nodeA, nodeB);
     }
 }
--- src/main/java/cn/edu/dll/struct/graph/utils/GraphUtils.java
+++ src/main/java/cn/edu/dll/struct/graph/utils/GraphUtils.java
@@ -10,18 +10,18 @@ import java.util.*;
 
 public class GraphUtils {
 
-    public static <T extends Edge> T getEdge(Graph<T> graph, Node nodeA, Node nodeB) {
-        Map<Node, T> neighboring = graph.getNeighboring(nodeA);
+    public static <V extends Number & Comparable<V>, E extends Edge<V>> E getEdge(Graph<V, E> graph, Node nodeA, Node nodeB) {
+        Map<Node, E> neighboring = graph.getNeighboring(nodeA);
         if (neighboring == null) {
             return null;
         }
         return neighboring.get(nodeB);
     }
 
-    public static <T extends Edge> Set<T> getEdgeSetByNodeSetAdjacent(Set<Node> nodeSet, Map<Node, Map<Node, T>> adjacentMap) {
-        Map<Node, T> tempInnerMap;
-        T tempEdge;
-        Set<T> edgeSet = new HashSet<>();
+    public static <V extends Number & Comparable<V>, E extends Edge<V>> Set<E> getEdgeSetByNodeSetAdjacent(Set<Node> nodeSet, Map<Node, Map<Node, E>> adjacentMap) {
+        Map<Node, E> tempInnerMap;
+        E tempEdge;
+        Set<E> edgeSet = new HashSet<>();
         for (Node leftNode : nodeSet) {
             tempInnerMap = adjacentMap.get(leftNode);
             for (Node rightNode : nodeSet) {
@@ -32,19 +32,27 @@ public class GraphUtils {
         return edgeSet;
     }
 
-    public static void showGraph(Graph<? extends Edge> graph) {
+    public static <E extends Edge<Double>> void edgeMultiple(Graph<Double, E> weightedGraph, Double factor) {
+        Set<E> edgeSet = weightedGraph.getEdgeSet();
+        for (E edge : edgeSet) {
+            edge.setValue(edge.getValue() * factor);
+        }
+    }
+
+    public static <V extends Number & Comparable<V>, E extends Edge<V>>
+    void showGraph(Graph<V, E> graph) {
         Node outerNode, innerNode;
-        Map<Node, ? extends Edge> innerMap;
-        Edge edge;
+        Map<Node, E> innerMap;
+        E edge;
         graph.getAdjacentMap();
         StringBuilder stringBuilder = new StringBuilder();
-        for (Map.Entry<Node, ? extends Map<Node, ? extends Edge>> nodeMapEntry : graph.getAdjacentMap().entrySet()) {
+        for (Map.Entry<Node, Map<Node, E>> nodeMapEntry : graph.getAdjacentMap().entrySet()) {
             outerNode = nodeMapEntry.getKey();
             innerMap = nodeMapEntry.getValue();
             stringBuilder.append(String.format("node(%d): ", outerNode.getNodeID()));
             int i = 0;
             int size = innerMap.size();
-            for (Map.Entry<Node, ? extends Edge> innerEntry : innerMap.entrySet()) {
+            for (Map.Entry<Node, E> innerEntry : innerMap.entrySet()) {
                 ++i;
                 innerNode = innerEntry.getKey();
                 edge = innerEntry.getValue();
